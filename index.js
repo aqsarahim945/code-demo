b8d4b7f97bdbd852a4c0d0fc286cb0038ed6e273
aqsarahim945 <aqsarahim945@gmail.com>
2025-09-09 05:10:18 +0000

    devops

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const message = process.env.MESSAGE || 'Hello Universe';
+const message = process.env.MESSAGE || 'Hello Devops';
 
 app.get('/', (req, res) => {
   res.send(message);
 });
 
-app.get('/hellouniverse', (req, res) => res.send('Hello Universe!'));
+app.get('/hellodevops', (req, res) => res.send('Hello Devops!'));
 app.get('/helloEarth', (req, res) => res.send('Hello Earth!'));
 
 app.listen(port, () => {
